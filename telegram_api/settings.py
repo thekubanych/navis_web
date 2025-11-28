207486148903bb658109bfa5f11a14ff0f584b19
thekubanych <kubanychmuhtarov>
2025-11-29 00:32:43 +0600

    ssf

--- telegram_api/settings.py
+++ telegram_api/settings.py
@@ -57,14 +57,14 @@ MIDDLEWARE = [
 
 # --- URLs ---
 ROOT_URLCONF = 'telegram_api.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -72,6 +72,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'telegram_api.wsgi.application'
 
